27b0dc61e7d35624ee95f0ef2298a06a85741180
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-31 12:34:25 -0800

    Implemented load_ratings in seed.py

--- seed.py
+++ seed.py
@@ -66,42 +66,42 @@ def load_movies():
     db.session.commit()
 
 
-# # def load_ratings():
-# #     """Load ratings from u.data into database."""
+def load_ratings():
+    """Load ratings from u.data into database."""
 
-#     print "Ratings"
+    print "Ratings"
 
-#     # Delete all rows in table, so if we need to run this a second time,
-#     # we won't be trying to add duplicate users
-#     Rating.query.delete()
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Rating.query.delete()
 
-#     # Read u.data file and insert data
-#     for row in open("seed_data/u.data"):
-#         row = row.rstrip()
-#         urating_id, age, gender, occupation, zipcode = row.split("|")
+    # Read u.data file and insert data
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        user_id, movie_id, score, _ = row.split("\t")
 
-#         user = User(user_id=user_id,
-#                     age=age,
-#                     zipcode=zipcode)
+        rating = Rating(user_id=user_id,
+                    movie_id=movie_id,
+                    score=score)
 
-#         # We need to add to the session or it won't ever be stored
-#         db.session.add(user)
+        # We need to add to the session or it won't ever be stored
+        db.session.add(rating)
 
-#     # Once we're done, we should commit our work
-#     db.session.commit()
+    # Once we're done, we should commit our work
+    db.session.commit()
 
 
-# def set_val_user_id():
-#     """Set value for the next user_id after seeding database"""
+def set_val_user_id():
+    """Set value for the next user_id after seeding database"""
 
-#     # Get the Max user_id in the database
-#     result = db.session.query(func.max(User.user_id)).one()
-#     max_id = int(result[0])
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(User.user_id)).one()
+    max_id = int(result[0])
 
-#     # Set the value for the next user_id to be max_id + 1
-#     query = "SELECT setval('users_user_id_seq', :new_id)"
-#     db.session.execute(query, {'new_id': max_id + 1})
-#     db.session.commit()
+    # Set the value for the next user_id to be max_id + 1
+    query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
 
 
 if __name__ == "__main__":
@@ -113,5 +113,5 @@ if __name__ == "__main__":
     # Import different types of data
     load_users()
     load_movies()
-    # load_ratings()
-    # set_val_user_id()
+    load_ratings()
+    set_val_user_id()
