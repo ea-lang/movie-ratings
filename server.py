850df9e141682ad9a57573022b01dec749da353b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-31 17:53:41 -0800

    Building registration form

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, jsonify, render_template, redirect, request, flash,
+                    session)
+
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,8 +24,43 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    
+    return render_template("homepage.html")
+
+
+@app.route('/users')
+def user_list():
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/register', methods=["GET"])
+def display_login_form():
+
+    return render_template("register_form.html")
+
+
+# @app.route('/register', methods=["POST"])
+# def process_login_form():
+#     email = request.form.get("email")
+#     password = request.form.get("password")
+
+
+#     user_check = User.query.filter_by(email=email).one()
+
+#         add user
+
+#     if user_check == []:
+#         user = User(email=email,
+#                     password=password)
+#         db.session.add(user)
+#         db.session.commit()
+#     elif:
+#         if password == user_check.password:
+#         pass
+#     #         # login
+#     # else:
+#         # passowrd does not match - back to form
 
 
 if __name__ == "__main__":
